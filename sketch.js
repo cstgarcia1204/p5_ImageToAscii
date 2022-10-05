395e9230d260e163d9583ff66979de3746cc3e3d
cstgarcia <ckstgarcia@gmail.com>
2022-10-05 03:35:07 -0600

    change the entire project from image to video conversion, just change code to work with video source

--- sketch.js
+++ sketch.js
@@ -1,30 +1,40 @@
-const density = 'Ñ@#W$9876543210?!abc;:+=-,._ ';
-let lula;  
-function preload() {
-  lula = loadImage('/Lula_meme-48.png');
-}
+// const density = 'Ñ@#W$9876543210?!abc;:+=-,._ '; //add more spaces build the image in a better definition,
+const density = '        .:-i|=+%O#@';
+// let lula;
+let video;
+let asciiDiv;
+// function preload() {
+//   lula = loadImage('/Lula_meme-48.png');
+// }
 
 function setup() {
   noCanvas();
+  video = createCapture(VIDEO);
+  video.size(48, 48);
+  asciiDiv = createDiv();
+}
 
-  background(0);
-  image(lula, 0, 0, width, height);
-  let w = width / lula.width;
-  let h = height / lula.height;
-  lula.loadPixels(); 
+
+  function draw() {
+  // background(0);
+  // image(lula, 0, 0, width, height);
+  // let w = width / lula.width;
+  // let h = height / lula.height;
+  video.loadPixels(); 
+  let asciiImage = '';
 
   // i = column, j = row
   //flip the loop that run to all matrix in horizontal trajectory or row by row
-  for(let j = 0; j < lula.height; j++) {
-    let row = ''; //acummulate all the characters into a string for each row
-    for(let i = 0; i < lula.width; i++){
+  for(let j = 0; j < video.height; j++) {
+    // let row = ''; //acummulate all the characters into a string for each row
+    for(let i = 0; i < video.width; i++){
 
       // formula to say column plus row * the width * 4 
       //  --> gives the red value for any given column and row pixel
-      const pixelIndex = (i+j * lula.width)*4;
-      const r = lula.pixels[pixelIndex + 0];
-      const g = lula.pixels[pixelIndex + 1];
-      const b = lula.pixels[pixelIndex + 2 ];
+      const pixelIndex = (i+j * video.width)*4;
+      const r = video.pixels[pixelIndex + 0];
+      const g = video.pixels[pixelIndex + 1];
+      const b = video.pixels[pixelIndex + 2 ];
       //to convert image to grayscale
       const avg = (r + g + b)/ 3;
       //from zero to length of the density
@@ -32,11 +42,12 @@ function setup() {
       const charIndex = floor(map(avg, 0, 255, len, 0)); //brightness value
 
       const c = density.charAt(charIndex);
-      if(c == ' ') row += '&nbps';
-      else row += c;
+      if(c == ' ') asciiImage += '&nbps;';
+      else asciiImage += c;
     }
-    createDiv(row);
+    asciiImage += '<br/>';
+    // createDiv(asciiImage);
     // console.log(row);
-
   }
+  asciiDiv.html(asciiImage);
 }

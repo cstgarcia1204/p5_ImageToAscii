1a5926b5aca5c88ce665109cdfb8fad1b12d33db
cstgarcia <ckstgarcia@gmail.com>
2022-10-05 02:47:26 -0600

    change formula to convert image to plane text, and add other sizes of image to test

--- sketch.js
+++ sketch.js
@@ -1,14 +1,12 @@
 const density = 'Ñ@#W$9876543210?!abc;:+=-,._ ';
 let lula;  
 function preload() {
-  lula = loadImage('/Lula_meme.png');
+  lula = loadImage('/Lula_meme-48.png');
 }
 
 function setup() {
-  createCanvas(400, 400);
-}
+  noCanvas();
 
-function draw() {
   background(0);
   image(lula, 0, 0, width, height);
   let w = width / lula.width;
@@ -16,8 +14,10 @@ function draw() {
   lula.loadPixels(); 
 
   // i = column, j = row
-  for(let i = 0; i < lula.width; i++) {
-    for(let j = 0; j < lula.height; j++){
+  //flip the loop that run to all matrix in horizontal trajectory or row by row
+  for(let j = 0; j < lula.height; j++) {
+    let row = ''; //acummulate all the characters into a string for each row
+    for(let i = 0; i < lula.width; i++){
 
       // formula to say column plus row * the width * 4 
       //  --> gives the red value for any given column and row pixel
@@ -27,20 +27,16 @@ function draw() {
       const b = lula.pixels[pixelIndex + 2 ];
       //to convert image to grayscale
       const avg = (r + g + b)/ 3;
-
-      noStroke();
-      fill(255);
-      // square(i * w, j * h, w);
-
       //from zero to length of the density
       const len = density.length;
       const charIndex = floor(map(avg, 0, 255, len, 0)); //brightness value
 
-      textSize(w);
-      textAlign(CENTER, CENTER);
-      text(density.charAt(charIndex), i * w + w * 0.5, j * h + h * 0.5);
+      const c = density.charAt(charIndex);
+      if(c == ' ') row += '&nbps';
+      else row += c;
+    }
+    createDiv(row);
+    // console.log(row);
 
-
-    }   
   }
 }
